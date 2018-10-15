0e63d8df24518aec616955a1809c9825b80cf897
Tristan Batchler <batchlertristan@gmail.com>
2018-10-15 22:31:40 +1000

    training data docs

--- NeuralNetwork/TrainingData.cs
+++ NeuralNetwork/TrainingData.cs
@@ -1,26 +1,46 @@
 using System;
 using System.Linq;
 using static Salty.Maths;
-using System.Collections;
 
 namespace Salty.AI {
 
+    /// <summary>Represents a set of training inputs and their corresponding 
+    /// outputs.</summary>
     public class TrainingData
     {
-        private Matrix inputs, expectedOutputs;
+        /// <summary>A matrix with rows representing training example inputs.
+        /// </summary>
+        private Matrix inputs;
+            
+        /// <summary>A matrix with rows representing training example expected 
+        /// outputs.</summary>
+        private Matrix expectedOutputs;
 
+        /// <summary>The number of training examples in this training data set.
+        /// </summary>
         public readonly int SampleSize;
 
+        /// <summary>The size of each input in this training data set.
+        /// </summary>
         public readonly int InputSize;
 
+        /// <summary>The size of each output in this training data set.
+        /// </summary>
         public readonly int OutputSize;
 
+        /// <summary>
+        /// Initialises a new training data set with the specified inputs and 
+        /// expected outputs.</summary>
+        /// <param name="inputs">The inputs as rows in a 2D array.</param>
+        /// <param name="expectedOutputs">The expected outputs as rows in a 2D 
+        /// array.</param>
         public TrainingData(float[,] inputs, float[,] expectedOutputs)
         {
-            /*if (inputs.GetLength(1) != expectedOutputs.GetLength(1))
+            if (inputs.GetLength(1) != expectedOutputs.GetLength(1))
             {
-                throw new ArgumentException("sample size must be equal for both input and expected output");
-            }*/
+                throw new ArgumentException("sample size must be equal for " +
+                    "both input and expected output");
+            }
 
             this.inputs = new Matrix(inputs);
             this.expectedOutputs = new Matrix(expectedOutputs);
@@ -29,12 +49,15 @@ namespace Salty.AI {
             this.OutputSize = this.expectedOutputs.ColumnCount;
         }
 
+        /// <summary>Shuffles the inputs and outputs in-place, retaining their 
+        /// correspondance.</summary>
+        /// <param name="rng">The random objected used to shuffle the current 
+        /// training data set.</param>
         public void Shuffle(Random rng)
         {
             int seed = rng.Next();
             Random r = new Random(seed);
-
-            Console.WriteLine("Shuffling training data");
+            
             float[] inputsArray = inputs.GetColumn(0);
             float[] outputsArray = expectedOutputs.GetColumn(0);
 
@@ -44,19 +67,52 @@ namespace Salty.AI {
             inputs.SetColumn(0, inputsArray);
             expectedOutputs.SetColumn(0, outputsArray);;
         }
-    
+
+        /// <summary>Returns the input at the given index into the current 
+        /// training data set.</summary>
+        /// <param name="i">The index into the current training data set to get 
+        /// the input at.</param>
+        /// <returns>The input at the given index into the current training 
+        /// data set.</returns>
         public float[] GetInput(int i)
         {
             return inputs.GetRow(i);
         }
 
+        /// <summary>Returns the expected output at the given index into the 
+        /// current training data set.</summary>
+        /// <param name="i">The index into the current training data set to get 
+        /// the expected output at.</param>
+        /// <returns>The expected output at the given index into the current 
+        /// training data set.</returns>
         public float[] GetExpectedOutput(int i)
         {
             return expectedOutputs.GetRow(i);
         }
 
+        /// <summary>Returns a subset of the current training data set, called 
+        /// a mini-batch.</summary>
+        /// <param name="startIndex">The starting index into the curren 
+        /// training data set to serve as the beginning of the mini-batch.
+        /// </param>
+        /// <param name="miniBatchSize">The size of the mini-batch to get.
+        /// </param>
+        /// <returns>The mini-batch starting at the given start index and of 
+        /// the given size.</returns>
         public TrainingData GetMiniBatch(int startIndex, int miniBatchSize)
         {
+            if (startIndex < 0 || startIndex >= SampleSize)
+            {
+                throw new ArgumentOutOfRangeException("mini-batch start " +
+                    "index is out of bounds");
+            }
+
+            if (startIndex + miniBatchSize > SampleSize)
+            {
+                throw new ArgumentOutOfRangeException("mini-batch size is " +
+                    "too large for the given start index");
+            }
+
             float[,] miniBatchInputs = new float[miniBatchSize, InputSize];
             float[,] miniBatchOutputs = new float[miniBatchSize, OutputSize];
             for (int i = 0; i < miniBatchSize; i++)
@@ -75,6 +131,10 @@ namespace Salty.AI {
             return new TrainingData(miniBatchInputs, miniBatchOutputs);
         }
 
+        /// <summary>Returns a <see cref="System.String" /> that represents 
+        /// the current training data set.</summary>
+        /// <returns>A <see cref="System.String" /> that represents the 
+        /// current training data set.</returns>
         public override string ToString()
         {
             return "INPUTS\n" + inputs.ToString() + 
